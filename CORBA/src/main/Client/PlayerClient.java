9158192203cf2c8841fbe20b8c69c9f78d9d1a73
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2020-06-24 17:58:00 -0400

    FEAT: Adding PLayer functions

--- CORBA/src/main/Client/PlayerClient.java
+++ CORBA/src/main/Client/PlayerClient.java
@@ -2,20 +2,40 @@ package main.Client;
 
 import DPSS_CORBA.GameServer;
 import DPSS_CORBA.GameServerHelper;
+import main.Constants.Constants;
+import main.Constants.Validations;
+import main.Model.Player;
+import main.Utilities.CustomLogger;
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 public class PlayerClient {
 
     static GameServer gameServerImpl;
 
-    public static void main(String[] args) {
+    private static BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
+    private static int client_IP_Address = 0;
+    private static int server_port_number = 0;
+    private static String client_server_name = "";
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
+    static FileHandler fileHandler = null;
+    
+    private static boolean setupCORBA(String[] arguments){
         try {
 
             // create and initialize the ORB
-            ORB orb = ORB.init(args, null);
+            ORB orb = ORB.init(arguments, null);
             // get the root naming context
             org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
             // Use NamingContextExt instead of NamingContext. This is part of the Interoperable naming Service.
@@ -30,7 +50,310 @@ public class PlayerClient {
         } catch (Exception e) {
             System.out.println("ERROR : " + e);
             e.printStackTrace(System.out);
+            return false;
+        }
+        
+        return true;
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        
+        // setup CORBA
+        setupCORBA(args);
+
+        boolean exit = false;
+
+        while (!exit) {
+
+            int userinput = showMenu();
+
+            switch (userinput) {
+
+                case 1:
+                    client_IP_Address = 0;
+                    Player newPlayer = createPlayer();
+                    String result1 = gameServerImpl.createPlayerAccount(newPlayer.getFirstName(), newPlayer.getLastName(), newPlayer.getAge(), newPlayer.getUserName(), newPlayer.getPassword(), newPlayer.getIPAddress());
+                    setupLogging(newPlayer.getUserName());
+                    LOGGER.info(newPlayer.getUserName() + " request to create new account on " + client_server_name);
+                    LOGGER.info(result1);
+                    if (fileHandler != null) fileHandler.close();
+                    System.out.println("Message: " +result1);
+
+                    break;
+
+                case 2:
+                    client_IP_Address = 0;
+                    System.out.print("Please enter user name: ");
+                    String userNameLogin = reader.readLine().trim();
+
+                    System.out.print("Please enter password: ");
+                    String password = reader.readLine().trim();
+
+                    while (!Validations.validateIP(client_IP_Address)) {
+
+                        System.out.print("Please enter IP starting (132, 93, 182): ");
+                        client_IP_Address = getValidIntegerInput();
+                    }
+                    getServerFromIP(client_IP_Address);
+
+
+                    System.out.println();
+
+                    setupLogging(userNameLogin);
+                    LOGGER.info(userNameLogin + " attempted to sign in on " + client_server_name);
+                    String result2 = gameServerImpl.playerSignIn(userNameLogin, password,
+                            String.valueOf(client_IP_Address));
+                    LOGGER.info(result2);
+                    System.out.println("Message: " +result2);
+                    if (fileHandler != null) fileHandler.close();
+
+
+                    break;
+
+                case 3:
+                    client_IP_Address = 0;
+                    System.out.print("Please enter user name: ");
+                    String userNameLogout = reader.readLine().trim();
+
+                    while (!Validations.validateIP(client_IP_Address)) {
+
+                        System.out.print("Please enter IP starting (132, 93, 182): ");
+                        client_IP_Address = getValidIntegerInput();
+                    }
+                    getServerFromIP(client_IP_Address);
+
+                    System.out.println();
+                    setupLogging(userNameLogout);
+                    LOGGER.info(userNameLogout + " attempted to sign out of " + client_server_name);
+
+
+                    String result3 = gameServerImpl.playerSignOut(userNameLogout, String.valueOf(client_IP_Address));
+                    LOGGER.info(result3);
+                    if (fileHandler != null) fileHandler.close();
+                    System.out.println("Message: " + result3);
+
+                    break;
+
+                case 4:
+
+                    client_IP_Address = 0;
+                    System.out.print("Please enter user name: ");
+                    String userNameTransferAcc = reader.readLine().trim();
+
+                    System.out.print("Please enter password: ");
+                    String passwordTransferAcc = reader.readLine().trim();
+
+                    int old_IP_Address = 0;
+                    int new_IP_Address = 0;
+
+                    while (!Validations.validateIP(old_IP_Address)) {
+
+                        System.out.print("Please enter Old IP: ");
+                        old_IP_Address = getValidIntegerInput();
+                    }
+                    getServerFromIP(old_IP_Address);
+
+                    while (!Validations.validateIP(new_IP_Address)) {
+
+                        System.out.print("Please enter New IP: ");
+                        new_IP_Address = getValidIntegerInput();
+                    }
+                    getServerFromIP(new_IP_Address);
+
+                    System.out.println();
+                    setupLogging(userNameTransferAcc);
+                    LOGGER.info(userNameTransferAcc + " attempted to transfer account from Old IP: " + old_IP_Address + " to New IP: " + new_IP_Address);
+
+                    String result4 = gameServerImpl.transferAccount(userNameTransferAcc, passwordTransferAcc, String.valueOf(old_IP_Address),String.valueOf(new_IP_Address));
+                    LOGGER.info(result4);
+                    if (fileHandler != null) fileHandler.close();
+                    System.out.println("Message: " + result4);
+
+                    break;
+
+                case 5:
+                    LOGGER.info("Exited the session.");
+                    if (fileHandler != null) fileHandler.close();
+                    System.out.println("Thank you for visiting our DPSS app");
+
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("Oops..! Invalid input. Please select 1,2,3 or 4 to perform required action");
+
+            }
+        }
+        
+        
+    }
+
+    // Return basic menu.
+    private static int showMenu() throws Exception {
+
+
+        System.out.println("\n**** Welcome to DPSS Game ****\n");
+
+        int userinput = 1;
+
+        System.out.println("Please select an option (1-4)");
+        System.out.println("1. Create new Player");
+        System.out.println("2. SignIn");
+        System.out.println("3. SignOut");
+        System.out.println("4. Transfer account to new IP Address");
+        System.out.println("5. Exit");
+
+        System.out.print("Please select an Option : ");
+
+        userinput = getValidIntegerInput();
+
+        return userinput;
+
+    }
+
+    /**
+     * createPlayer. - Validates and creates Player object
+     *
+     * @throws  Exception
+     */
+    private static Player createPlayer() throws Exception {
+
+        // inputting first name
+        System.out.print("Please enter first name: ");
+        String firstName = reader.readLine().trim();
+
+        // inputting last name
+        System.out.print("Please enter last name: ");
+        String lastName = reader.readLine().trim();
+
+        // inputting age
+        boolean ageInt = false;
+        int age = 0;
+
+        do {
+            try {
+                System.out.print("Please enter your age: ");
+                age = getValidIntegerInput();
+                ageInt = true;
+            } catch (Exception e) {
+                System.out.println("Invalid age");
+            }
+        } while (!ageInt & Validations.validateAge(age));
+
+        // inputting username
+        System.out.print("Please enter a unique username: ");
+        String userName = reader.readLine();
+        while (!Validations.validateUserName(userName)) {
+            System.out.println("Error: Username must be between 5 to 15 characters");
+            System.out.print("Please enter a unique username: ");
+            userName = reader.readLine().trim();
+        }
+
+        // inputting password
+        System.out.print("Please enter password: ");
+        String password = reader.readLine().trim();
+
+        while (!Validations.validatePassword(password)) {
+            System.out.println("Error: Password must be minimum 6 characters");
+            System.out.print("Please enter password: ");
+            password = reader.readLine().trim();
+        }
+
+
+        while (!Validations.validateIP(client_IP_Address)) {
+
+            System.out.print("Please enter IP starting (132, 93, 182): ");
+            client_IP_Address = getValidIntegerInput();
+        }
+        getServerFromIP(client_IP_Address);
+
+
+        System.out.println();
+
+        return new Player(firstName, lastName, age, userName, password, String.valueOf(client_IP_Address), false);
+
+    }
+
+    /**
+     * getServerFromIP. - This method takes IP by user and gets RMI registry
+     *
+     * @param client_IP_Address IP address entered by user
+     */
+    private static void getServerFromIP(int client_IP_Address){
+
+
+        switch (client_IP_Address) {
+
+            case 132:
+                client_server_name = Constants.SERVER_NAME_AMERICA;
+                server_port_number = Constants.SERVER_PORT_AMERICA;
+                break;
+
+            case 93:
+                client_server_name = Constants.SERVER_NAME_EUROPE;
+                server_port_number = Constants.SERVER_PORT_EUROPE;
+                break;
+
+            case 182:
+                client_server_name = Constants.SERVER_NAME_ASIA;
+                server_port_number = Constants.SERVER_PORT_ASIA;
+                break;
+            default:
+                System.out.println("Error: Invalid server IP");
+
 
         }
+
+        try {
+            System.out.println("***** Verifying info from server. Please wait. *****");
+            Registry registry = LocateRegistry.getRegistry(server_port_number);
+
+            //TODO: server
+            //gameServerImpl = (DPSS_GameServerInterface) registry.lookup(client_server_name);
+
+        }catch (Exception e){
+            System.out.println( e.getLocalizedMessage());
+        }
     }
+
+    /**
+     * getValidIntegerInput. - Takes input from console and validates proper integer
+     *
+     */
+    private static int getValidIntegerInput() {
+
+        int value = 0;
+        boolean inputValid = false;
+        do {
+            try {
+                String input = reader.readLine().trim();
+                if(input.contains(".")){
+                    input = input.split("\\.")[0];
+                }
+                value = Integer.valueOf(input);
+                inputValid = true;
+
+            } catch (Exception e) {
+                System.out.println("Error: This field requires a number value. Please try again");
+            }
+        } while (!inputValid);
+
+        return value;
+    }
+
+    /**
+     * setupLogging. - Setup logger for the class
+     * @param name - to create log file for player username
+     */
+    private static void setupLogging(String name) throws IOException {
+        File files = new File(Constants.PLAYER_LOG_DIRECTORY);
+        if (!files.exists())
+            files.mkdirs();
+        files = new File(Constants.PLAYER_LOG_DIRECTORY+"Player_"+ name+ ".log");
+        if(!files.exists())
+            files.createNewFile();
+        fileHandler = CustomLogger.setup(files.getAbsolutePath());
+    }
+
 }
---
+++ CORBA/src/main/Constants/Constants.java
@@ -0,0 +1,25 @@
+package main.Constants;
+
+public class Constants {
+
+    /* Game server ports */
+    public static final int SERVER_PORT_AMERICA = 2421;
+    public static final int SERVER_PORT_EUROPE = 5892;
+    public static final int SERVER_PORT_ASIA = 8091;
+
+    /* Game server IPs */
+    public static final int SERVER_IP_AMERICA = 132;
+    public static final int SERVER_IP_EUROPE = 93;
+    public static final int SERVER_IP_ASIA = 182;
+
+    /* Game server names */
+    public static final String SERVER_NAME_AMERICA = "AmericaGameServer";
+    public static final String SERVER_NAME_EUROPE = "EuropeGameServer";
+    public static final String SERVER_NAME_ASIA = "AsiaGameServer";
+
+    /* Log storage locations */
+    public static final String SERVER_LOG_DIRECTORY = "./src/Logs/Server/";
+    public static final String PLAYER_LOG_DIRECTORY = "./src/Logs/Player/";
+    public static final String ADMIN_LOG_DIRECTORY = "./src/Logs/Admin/";
+
+}
