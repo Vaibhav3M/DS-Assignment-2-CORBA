9a92c634ce29cb2895e4b080ab52e0f3dc95c72e
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2020-06-24 20:39:09 -0400

    FEAT: Adding server functionalities

--- CORBA/src/main/Server/America/AmericanServer.java
+++ CORBA/src/main/Server/America/AmericanServer.java
@@ -3,7 +3,7 @@ package main.Server.America;
 import DPSS_CORBA.GameServer;
 import DPSS_CORBA.GameServerHelper;
 import main.Constants.Constants;
-import main.Server.Asia.AsianServerImpl;
+import main.Utilities.CustomLogger;
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContextExt;
@@ -11,21 +11,103 @@ import org.omg.CosNaming.NamingContextExtHelper;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
 public class AmericanServer {
 
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    // to manage log files
+    static FileHandler fileHandler = null;
+
+    /**
+     *Recieve - Setup UDP server to recieve requests.
+     *
+     * @param serverImpl the server
+     */
+    public static void recieve(AmericanServerImpl serverImpl) {
+
+        String responseString = "";
+        DatagramSocket dataSocket = null;
+
+        try {
+
+            dataSocket = new DatagramSocket(Constants.SERVER_PORT_ASIA);
+            byte[] buffer = new byte[1000];
+            LOGGER.info( "Server started..!!!");
+            System.out.println(Constants.SERVER_NAME_ASIA + " started at port " + Constants.SERVER_PORT_ASIA);
+            while (true) {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                dataSocket.receive(request);
+                String requestMessage = new String(request.getData(),0,request.getLength());
+
+                LOGGER.info("Received UDP request message: " + requestMessage);
+
+                String request_IP = requestMessage.split(":")[0];
+                requestMessage = requestMessage.split(":")[1];
+
+                if (requestMessage.split("=")[0].equalsIgnoreCase("username")) {
+                    responseString = serverImpl.playerSignOut(requestMessage.split("=")[1],request_IP);
+                } else {
+                    responseString = serverImpl.getPlayerStatus("Admin", "Admin", String.valueOf(request.getPort()), false);
+                }
+
+                LOGGER.info("Sent UDP response message: " + responseString);
+                DatagramPacket reply = new DatagramPacket(responseString.getBytes(), responseString.length(), request.getAddress(), request.getPort());
+
+                dataSocket.send(reply);
+            }
+
+        } catch (SocketException e) {
+            LOGGER.info("Exception at socket" +e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOGGER.info("Exception at IO" +e.getLocalizedMessage());
+        } finally {
+            if (dataSocket != null) dataSocket.close();
+            if (fileHandler != null) fileHandler.close();
+        }
+
+    }
+
     public static void main(String args[]) {
+
+        AmericanServerImpl serverImplementation = new AmericanServerImpl(LOGGER);
+
+        Thread server_america = new Thread(()->
+        {
+            try {
+                //setup logger
+                setupLogging();
+                //UDP setup
+                recieve(serverImplementation);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Exception at main" +e.getLocalizedMessage());
+            }
+        });
+
+        server_america.setName("thread_America_server");
+        server_america.start();
+
         // create and initialize the ORB
         ORB orb = ORB.init(args, null);
         try {
             // get reference to rootpoa & activate the POAManager
             POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
             rootpoa.the_POAManager().activate();
+
             // create servant and register it with the ORB
-            AmericanServerImpl americanServer = new AmericanServerImpl();
-            americanServer.setORB(orb);
+            serverImplementation.setORB(orb);
 
             // get object reference from the servant
-            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(americanServer);
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(serverImplementation);
 
             GameServer href = GameServerHelper.narrow(ref);
             // get the root naming context
@@ -46,6 +128,19 @@ public class AmericanServer {
         }
         orb.shutdown(false);
         System.out.println("AmericanServer Exiting ...");
+    }
+
+    /**
+     * setupLogging. - Setup logger for the class
+     */
+    private static void setupLogging() throws IOException {
+        File files = new File(Constants.SERVER_LOG_DIRECTORY);
+        if (!files.exists())
+            files.mkdirs();
+        files = new File(Constants.SERVER_LOG_DIRECTORY+"ASIA_Server.log");
+        if(!files.exists())
+            files.createNewFile();
+        fileHandler = CustomLogger.setup(files.getAbsolutePath());
     }
 }
 
--- CORBA/src/main/Server/America/AmericanServerImpl.java
+++ CORBA/src/main/Server/America/AmericanServerImpl.java
@@ -1,32 +1,211 @@
 package main.Server.America;
 
 import DPSS_CORBA.GameServerPOA;
+import main.Constants.Constants;
+import main.Model.Player;
+import main.SendUDP.SendReceiveUDPMessage;
 import org.omg.CORBA.ORB;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
 
 public class AmericanServerImpl extends GameServerPOA {
 
     private ORB orb;
-    public void setORB(ORB orb_val) { orb = orb_val;
+    private static final long serialVersionUID = 7526472295622776147L;
+
+    //lock to enable synchronization
+    private static Lock lock = new ReentrantLock(true);
+    //to store player info
+    private static Hashtable<Character, ArrayList<Player>> playersTable = new Hashtable<>();
+    //to log activities in a log file
+    private static Logger LOGGER;
+
+    protected AmericanServerImpl(Logger logger) {
+        super();
+        LOGGER = logger;
+        addDummyData();
     }
+
+    // set ORB value
+    public void setORB(ORB orb_val) { orb = orb_val; }
 
     @Override
     public String createPlayerAccount(String FirstName, String LastName, float Age, String Username, String Password, String IPAddress) {
-        return null;
+        //create player object
+        Player player = new Player(FirstName, LastName, Math.round(Age) , Username, Password, IPAddress, false);
+
+        LOGGER.info("Received RMI request - Create Player - " + player.toString());
+
+        //check if username exists
+        if (checkUserName(player.getUserName())) {
+
+            LOGGER.info("Username=" + player.getUserName() + " already existed");
+            return "Username already exists";
+        }
+
+        char playerKey = player.getUserName().charAt(0);
+        ArrayList<Player> playerList;
+
+        try {
+            // lock while performing operations
+            lock.lock();
+
+            if (playersTable.containsKey(playerKey)) {
+
+                playerList = playersTable.get(playerKey);
+
+                for (int i = 0; i < playerList.size(); i++) {
+                    Player currPlayer = playerList.get(i);
+
+                    if (currPlayer.getUserName().equalsIgnoreCase(player.getUserName())) {
+                        LOGGER.info("Username=" + player.getUserName() + " already existed");
+
+                        return "UserName already exists";
+                    }
+                }
+                playerList.add(player);
+            } else {
+                playerList = new ArrayList<>();
+                playerList.add(player);
+                playersTable.put(playerKey, playerList);
+            }
+        } finally {
+            //unlock once operation complete
+            lock.unlock();
+        }
+
+        LOGGER.info("Player Created successfully - " + player.toString());
+
+        return "Successful";
     }
 
     @Override
     public String playerSignIn(String Username, String Password, String IPAddress) {
-        return null;
+
+        LOGGER.info("Received RMI request - SignIn Player - " + "Username=" + Username);
+
+        char playerKey = Username.charAt(0);
+
+        try {
+            // lock while performing operations
+            lock.lock();
+            if (playersTable.containsKey(playerKey)) {
+
+                ArrayList<Player> playerList = playersTable.get(playerKey);
+
+                for (int i = 0; i < playerList.size(); i++) {
+                    Player currPlayer = playerList.get(i);
+                    if (currPlayer.getUserName().equalsIgnoreCase(Username) && currPlayer.getPassword().equalsIgnoreCase(Password)) {
+
+                        if (currPlayer.isSignedIn()) {
+                            LOGGER.info("Player is already SignedIn - " + "Username=" + Username);
+                            return currPlayer.getUserName() + " is already logged in.";
+                        }
+
+                        currPlayer.setSignedIn(true);
+                        playerList.remove(i);
+                        playerList.add(currPlayer);
+                        playersTable.put(playerKey, playerList);
+
+                        LOGGER.info("Player SignedIn - " + "Username=" + Username);
+                        return currPlayer.getUserName() + " has logged in.";
+                    }
+                }
+            } else {
+
+                LOGGER.info("Player not found - " + "Username=" + Username);
+                return Username + " not found";
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        return Username + " not found";
     }
 
     @Override
     public String playerSignOut(String Username, String IPAddress) {
-        return null;
+        boolean isFromServerIP = (Integer.parseInt(IPAddress) == Constants.SERVER_IP_ASIA);
+
+        char playerKey = Username.charAt(0);
+
+        try {
+            // lock while performing operations
+            lock.lock();
+            if (playersTable.containsKey(playerKey)) {
+
+                ArrayList<Player> playerList = playersTable.get(playerKey);
+
+                for (int i = 0; i < playerList.size(); i++) {
+                    Player currPlayer = playerList.get(i);
+                    if (currPlayer.getUserName().equalsIgnoreCase(Username)) {
+
+                        if (isFromServerIP) {
+                            LOGGER.info("Received RMI request - SignOut Player - " + Username);
+
+                            if (!currPlayer.isSignedIn()) {
+                                LOGGER.info("Player is not SignedIn - " + "Username=" + Username);
+                                return currPlayer.getUserName() + " is not signed in.";
+                            }
+                            currPlayer.setSignedIn(false);
+                            playerList.remove(i);
+                            playerList.add(currPlayer);
+                            playersTable.put(playerKey, playerList);
+                        }
+                        LOGGER.info("Player SignedOut - " + "Username=" + Username);
+
+                        return currPlayer.getUserName() + " has logged out.";
+                    }
+                }
+            } else {
+                LOGGER.info("Player not found - " + "Username=" + Username);
+                return "User not found";
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        return "User not found";
     }
 
     @Override
     public String getPlayerStatus(String AdminUsername, String AdminPassword, String IPAddress, boolean checkOtherServers) {
-        return null;
+        if (!AdminUsername.equalsIgnoreCase("Admin") || !AdminPassword.equalsIgnoreCase("Admin")) {
+            return "Username or password incorrect.";
+        }
+
+        String response = "AS: ";
+        int onlineCount = 0;
+        int offlineCount = 0;
+
+        try {
+            lock.lock();
+            for (char key : playersTable.keySet()) {
+                for (Player p : playersTable.get(key)) {
+                    if (p.isSignedIn()) onlineCount++;
+                    else offlineCount++;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        String response_America = "";
+        String response_Europe = "";
+
+        //Send UDP requests to other servers
+        if (checkOtherServers) {
+            response_America = getPlayerStatusUDP(Constants.SERVER_PORT_AMERICA);
+            response_Europe = getPlayerStatusUDP(Constants.SERVER_PORT_EUROPE);
+        }
+
+        //append the results
+        response = response + onlineCount + " online, " + offlineCount + " offline. " + response_America + response_Europe;
+        return response;
     }
 
     @Override
@@ -38,4 +217,90 @@ public class AmericanServerImpl extends GameServerPOA {
     public String suspendAccount(String AdminUsername, String AdminPassword, String AdminIP, String UsernameToSuspend) {
         return null;
     }
+
+    /**
+     * getPlayerStatusUDP.
+     *
+     * @param serverPort - port to which UDP request is sent
+     * @return the UDP response
+     */
+    private String getPlayerStatusUDP(int serverPort) {
+
+        LOGGER.info("Created UDP request - Get player status from port " + serverPort);
+        String[] response = {"No response from " + serverPort};
+
+        SendReceiveUDPMessage sendReceiveUDPMessage = new SendReceiveUDPMessage();
+
+        //create a new thread for UDP request
+        Thread UDPThread = new Thread(() ->
+        {
+            try {
+                response[0] = sendReceiveUDPMessage.getUDPResponse("playerstatus", serverPort, Constants.SERVER_PORT_ASIA);
+
+            } catch (Exception e) {
+                System.out.println("Exception at getplayerstatus" + e.getLocalizedMessage());
+            }
+
+        });
+
+        UDPThread.setName("Thread - UDP " + serverPort);
+        UDPThread.start();
+
+        try {
+            UDPThread.join();
+        } catch (Exception e) {
+            System.out.println("Exception at getplayerstatus" + e.getLocalizedMessage());
+        }
+        LOGGER.info("Received UDP response from " + serverPort + " - " + response[0]);
+        return response[0];
+
+    }
+
+    /**
+     * checkUserName - to check if username exists on other servers using UDP
+     *
+     * @param userName - username to check
+     * @return  username status
+     */
+    private boolean checkUserName(String userName) {
+        SendReceiveUDPMessage sendReceiveUDPMessage = new SendReceiveUDPMessage();
+
+        String check_american = sendReceiveUDPMessage.getUDPResponse("UserName=" + userName, Constants.SERVER_PORT_AMERICA, Constants.SERVER_PORT_ASIA);
+        String check_europe = sendReceiveUDPMessage.getUDPResponse("UserName=" + userName, Constants.SERVER_PORT_EUROPE, Constants.SERVER_PORT_ASIA);
+
+        return !check_american.equalsIgnoreCase("User not found") || !check_europe.equalsIgnoreCase("User not found");
+    }
+
+    private void addDummyData() {
+        addDummyDataHelper(new Player("Test", "Test", 21, "Test_Asia", "test123", String.valueOf(Constants.SERVER_IP_AMERICA), false));
+        addDummyDataHelper(new Player("Yin", "Li", 21, "YinLi2", "yinli2", String.valueOf(Constants.SERVER_IP_AMERICA), true));
+    }
+
+    private void addDummyDataHelper(Player player){
+
+        char playerKey = player.getUserName().charAt(0);
+
+        ArrayList<Player> playerList;
+
+        try {
+            lock.lock();
+
+            if (playersTable.containsKey(playerKey)) {
+
+                playerList = playersTable.get(playerKey);
+
+                playerList.add(player);
+
+            } else {
+                playerList = new ArrayList<>();
+                playerList.add(player);
+                playersTable.put(playerKey, playerList);
+
+            }
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
 }
